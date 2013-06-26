9cefaf6cfc4c67f35397aaf8d6d873d1b284d7aa
Jeremy Dillworth <jdillworth@gmail.com>
2013-06-26 15:42:15 -0400

    Modifying settings to work with any path. Adding wsgi.py file for webfaction environment.

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
-# Django settings for minimalpaypal project.
+import os
 
+FILE_PATH = os.path.split(__file__)[0]
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'minimalpaypal.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(FILE_PATH, 'minimalpaypal.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -145,7 +146,7 @@ LOGGING = {
     }
 }
 
-with open('api-keys.txt') as f:
+with open(os.path.join(FILE_PATH, 'api-keys.txt')) as f:
     from ConfigParser import ConfigParser
     cfp = ConfigParser()
 
